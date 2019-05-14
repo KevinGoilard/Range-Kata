e591667b5e25fa0018bca8434320505bc90ef06e
KevinGoilard <kevin.goilard@gmail.com>
2019-05-14 15:21:18 +0200

    first contains case

---
+++ intervals/intervals.py
@@ -0,0 +1,9 @@
+
+
+class Interval:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def contains(self, param):
+        return True
--- tests/intervalstests.py
+++ tests/intervalstests.py
@@ -1,6 +1,14 @@
 import unittest
+from intervals.intervals import Interval
 
 
 class IntervalsTests(unittest.TestCase):
     def test_true(self):
         self.assertTrue(True)
+
+    def test_interval_contains_return_true(self):
+        interval = Interval(2, 6)
+
+        result = interval.contains({2, 4})
+
+        self.assertTrue(result)
